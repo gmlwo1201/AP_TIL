81640926a82d2a074c44a2c766d4b36de3578d87
gmlwo1201 <62641744+gmlwo1201@users.noreply.github.com>
2025-03-17 22:33:43 +0900

    Update 요일_출력.dart

--- 요일_출력.dart
+++ 요일_출력.dart
@@ -1,7 +1,7 @@
 void main() {
-    var input = '2025-03-11';
-    var date = DateTime.parse(input);
-    List<String> week = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'];
+    var input = '2025-03-11'; // 날짜 지정
+    var date = DateTime.parse(input); // 날짜를 숫자열에서 함수로 지정
+    List<String> week = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']; // 요일로 표시하기 위해 리스트 생성
 
     print('${week[date.weekday - 1]}');
 }
